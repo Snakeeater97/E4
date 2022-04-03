ec6992d16add2bc7cab00c81ee6402ac31d4c230
Snakeeater97 <14RO40550353@campus.economicas.uba.ar>
2022-04-03 11:26:34 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escriba frases, el programa se cerrara al escribir fin");
+string texto = Console.ReadLine();
+while (texto != "fin" )
+{
+    Console.WriteLine("Escriba frases, el programa se cerrara al escribir fin");
+    texto = Console.ReadLine();
+}
